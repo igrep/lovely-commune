a126b5834ddfa9c71f63c4fe6de5bb9b2d6cdaaf
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-11-23 23:36:33 +0900

    :construction: WIP: fill heart color on tracing
    Still has a bug that the heart state does not get reset after tracing the L

--- src/Heart.elm
+++ src/Heart.elm
@@ -6,6 +6,7 @@ import Signal
 import StartApp.Simple
 import Svg exposing (..)
 import Svg.Attributes as A
+import Svg.Events as E
 
 
 type alias Model =
@@ -136,34 +137,89 @@ tracePart p hs =
     CenterRight -> { hs | centerRight = On }
 
 
+nextPart : Model -> Maybe Part
+nextPart m =
+  case List.head m.leftActions of
+    Just (Trace part) -> Just part
+    -- Just Reset -> nextPart m
+    _ -> Nothing
+
+
 view : Signal.Address Action -> Model -> Html
 view a m =
-  let maybeNext = List.head m.leftActions
+  let maybeNextPart = nextPart m
 
       left =
-        viewPart "path4167" "m 77.832545,73.499417 c -36.282713,33.405363 -56.47197,71.961443 -57,131.000003 1,82 57,136 102.303875,190.34806 19.31275,-58.94291 20.69612,-118.34806 13.37467,-178.9431 -5.77813,-47.82183 -25.61233,-94.70957 -58.678545,-142.404963 z"
+        viewPart
+          a
+          "path4167"
+          "m 77.832545,73.499417 c -36.282713,33.405363 -56.47197,71.961443 -57,131.000003 1,82 57,136 102.303875,190.34806 19.31275,-58.94291 20.69612,-118.34806 13.37467,-178.9431 -5.77813,-47.82183 -25.61233,-94.70957 -58.678545,-142.404963 z"
+          Left
+          maybeNextPart
+          m.heartState.left
 
       right =
-        viewPart "path4169" "m 502.20905,72.801742 c 36.28271,33.405368 56.47196,71.961448 56.99999,131.000008 -1,82 -56.99999,136 -102.30387,190.34806 -19.31275,-58.94291 -20.69612,-118.34806 -13.37467,-178.9431 5.77813,-47.82183 25.61233,-94.70957 58.67855,-142.404968 z"
+        viewPart
+          a
+          "path4169"
+          "m 502.20905,72.801742 c 36.28271,33.405368 56.47196,71.961448 56.99999,131.000008 -1,82 -56.99999,136 -102.30387,190.34806 -19.31275,-58.94291 -20.69612,-118.34806 -13.37467,-178.9431 5.77813,-47.82183 25.61233,-94.70957 58.67855,-142.404968 z"
+          Right
+          maybeNextPart
+          m.heartState.right
 
       centerLeft =
-        viewPart "path4171" "m 290.83255,234.49942 c -13.00001,-19 -45.00137,-26.07131 -65.3123,-25.64765 -40.9411,0.85398 -67.18703,18.45477 -70.0893,33.44685 -0.59841,38.2008 71.1193,69.2008 135.4016,95.2008"
+        viewPart
+          a
+          "path4171"
+          "m 290.83255,234.49942 c -13.00001,-19 -45.00137,-26.07131 -65.3123,-25.64765 -40.9411,0.85398 -67.18703,18.45477 -70.0893,33.44685 -0.59841,38.2008 71.1193,69.2008 135.4016,95.2008"
+          CenterLeft
+          maybeNextPart
+          m.heartState.centerLeft
 
       centerRight =
-        viewPart "path4181" "m 290.12989,234.49942 c 13.00001,-19 45.00137,-26.07131 65.3123,-25.64765 40.9411,0.85398 67.18703,18.45477 70.0893,33.44685 0.59841,38.2008 -71.1193,69.2008 -135.4016,95.2008"
+        viewPart
+          a
+          "path4181"
+          "m 290.12989,234.49942 c 13.00001,-19 45.00137,-26.07131 65.3123,-25.64765 40.9411,0.85398 67.18703,18.45477 70.0893,33.44685 0.59841,38.2008 -71.1193,69.2008 -135.4016,95.2008"
+          CenterRight
+          maybeNextPart
+          m.heartState.centerRight
 
       upperLeft =
-        viewPart "path4183" "m 290.83255,35.499417 c -22.00001,-12 -63.00001,-17 -102.67112,-12.579008 -32.10606,3.577934 -63.32889,17.579008 -99.049567,41.365788 23.458807,34.093405 44.232597,70.404873 54.720687,115.213223 35.99999,-45 92.31217,-65.17868 147,-35"
+        viewPart
+          a
+          "path4183"
+          "m 290.83255,35.499417 c -22.00001,-12 -63.00001,-17 -102.67112,-12.579008 -32.10606,3.577934 -63.32889,17.579008 -99.049567,41.365788 23.458807,34.093405 44.232597,70.404873 54.720687,115.213223 35.99999,-45 92.31217,-65.17868 147,-35"
+          UpperLeft
+          maybeNextPart
+          m.heartState.upperLeft
 
       upperRight =
-        viewPart "path4185" "m 289.9722,35.499417 c 22.00001,-12 63.00001,-17 102.67112,-12.579008 32.10606,3.577934 63.32889,17.579008 99.04957,41.365788 -23.45881,34.093405 -44.2326,70.404873 -54.72069,115.213223 -35.99999,-45 -92.31217,-65.17868 -147,-35"
+        viewPart
+          a
+          "path4185"
+          "m 289.9722,35.499417 c 22.00001,-12 63.00001,-17 102.67112,-12.579008 32.10606,3.577934 63.32889,17.579008 99.04957,41.365788 -23.45881,34.093405 -44.2326,70.404873 -54.72069,115.213223 -35.99999,-45 -92.31217,-65.17868 -147,-35"
+          UpperRight
+          maybeNextPart
+          m.heartState.upperRight
 
       bottomLeft =
-        viewPart "path4189" "m 290.83255,433.62936 c -60.00001,-30.12994 -111.77329,-81.52088 -138,-136.12994 -3.7496,37.3272 -4.1263,76.98949 -17,108 21.77107,19.87863 38.23442,34.67652 55.1925,47.21311 51.98861,38.4336 66.80749,46.92907 99.8075,64.78689"
+        viewPart
+          a
+          "path4189"
+          "m 290.83255,433.62936 c -60.00001,-30.12994 -111.77329,-81.52088 -138,-136.12994 -3.7496,37.3272 -4.1263,76.98949 -17,108 21.77107,19.87863 38.23442,34.67652 55.1925,47.21311 51.98861,38.4336 66.80749,46.92907 99.8075,64.78689"
+          BottomLeft
+          maybeNextPart
+          m.heartState.bottomLeft
 
       bottomRight =
-        viewPart "path4191" "m 290.33254,433.62936 c 60.00001,-30.12994 111.77329,-81.52088 138,-136.12994 3.7496,37.3272 4.1263,76.98949 17,108 -21.77107,19.87863 -38.23442,34.67652 -55.1925,47.21311 -51.98861,38.4336 -66.80749,46.92907 -99.8075,64.78689"
-
+        viewPart
+          a
+          "path4191"
+          "m 290.33254,433.62936 c 60.00001,-30.12994 111.77329,-81.52088 138,-136.12994 3.7496,37.3272 4.1263,76.98949 17,108 -21.77107,19.87863 -38.23442,34.67652 -55.1925,47.21311 -51.98861,38.4336 -66.80749,46.92907 -99.8075,64.78689"
+          BottomRight
+          maybeNextPart
+          m.heartState.bottomRight
 
   in
     svg
@@ -179,12 +235,27 @@ view a m =
       ]
 
 
-viewPart : String -> String -> Svg
-viewPart svgId svgD =
-  let partStyle =
-        A.style "fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+viewPart : Signal.Address Action -> String -> String -> Part -> Maybe Part -> PartState -> Svg
+viewPart a svgId svgD drawnPart nextPart drawnPartState =
+  let onMouseOver =
+        if ( Just drawnPart ) == nextPart
+          then [ E.onMouseOver <| Signal.message a ( Trace drawnPart ) ]
+          else []
+
+      fillStyle =
+        if drawnPartState == On
+          then "fill:#ff0000;fill-opacity:1;"
+          else "fill:#ffffff;fill-opacity:0;"
+      partStyle = A.style <| fillStyle ++ "fill-rule:evenodd;stroke:#ff0000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
   in
-  path [partStyle, A.d svgD, A.id svgId] []
+  path ( onMouseOver ++ [partStyle, A.d svgD, A.id svgId] ) []
+
+
+isTurnedOn : PartState -> Bool
+isTurnedOn p =
+  case p of
+    On -> True
+    _  -> False
 
 
 join : List a -> List (List a) -> List a
