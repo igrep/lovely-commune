c9b3b987e2f712db50d56043ddef37c0ab439867
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-12-13 20:10:47 +0900

    :art: refactor Position.elm

--- src/Position.elm
+++ src/Position.elm
@@ -1,9 +1,11 @@
 module Position
   ( mouseTouch
   , Position
+  , PointedElementInfo
   , pointedElementId
   ) where
 
+import Maybe
 import Mouse
 import Signal exposing (Signal)
 import Touch
@@ -12,10 +14,47 @@ import Task exposing (Task, andThen)
 import Native.Position
 
 
-type alias Position = Maybe (Int, Int)
+type alias Position = (Int, Int)
+
+type alias ElementId = String
+
+type alias PointedElementInfo =
+  { position  : Position
+  , elementId : ElementId
+  }
 
 
-mouseTouch : Signal Position
+pointedElementId : Signal.Address PointedElementInfo -> Signal (Task x ())
+pointedElementId address =
+  Signal.map (sendPointedElement address) mouseTouch
+
+
+sendPointedElement : Signal.Address PointedElementInfo -> Maybe Position -> Task x ()
+sendPointedElement address maybePosition =
+  getPointedElement maybePosition
+    `andThen` \maybeElement ->
+      case maybeElement of
+        Just element -> Signal.send address element
+        _            -> noOp
+
+
+getPointedElement : Maybe Position -> Task x (Maybe PointedElementInfo)
+getPointedElement maybePosition =
+  case maybePosition of
+    Just position ->
+      (Task.map << Maybe.map)
+        (PointedElementInfo position)
+        (getIdFromPoint position)
+    _ ->
+      Task.succeed Nothing
+
+
+getIdFromPoint : Position -> Task x (Maybe String)
+getIdFromPoint (x, y) =
+  Native.Position.getIdFromPoint x y
+
+
+mouseTouch : Signal (Maybe Position)
 mouseTouch =
   let mouse = Signal.map Just Mouse.position
       touch =
@@ -24,26 +63,11 @@ mouseTouch =
   Signal.merge mouse touch
 
 
-pointedElementId : Signal.Address String -> Signal (Task x ())
-pointedElementId address =
-  Signal.map (sendPointedElementId address) mouseTouch
-
-
-sendPointedElementId : Signal.Address String -> Position -> Task x ()
-sendPointedElementId address position =
-  getIdFromPoint position
-    `andThen` \maybeId ->
-      case maybeId of
-        Just id -> Signal.send address id
-        _       -> Task.succeed ()
-
-
 toPair : Touch.Touch -> (Int, Int)
-toPair t = (t.x, t.y)
+toPair t =
+  (t.x, t.y)
 
 
-getIdFromPoint : Position -> Task x (Maybe String)
-getIdFromPoint position =
-  case position of
-    Just (x, y) -> Native.Position.getIdFromPoint x y
-    _           -> Task.succeed Nothing
+noOp : Task x ()
+noOp =
+  Task.succeed ()
